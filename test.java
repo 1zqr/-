f8748a996413c67058ddfd82f779c3749a6403d3
1zqr <90433868+1zqr@users.noreply.github.com>
2021-09-26 22:35:03 +0800

    Create test.java

---
+++ test.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void mathmaticnumber() {
+
+    }
+}
